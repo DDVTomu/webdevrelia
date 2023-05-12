d80d98fb605e0b6ac5161edf3e1e25171fb41b37
DDVTomu <phan031099@gmail.com>
2023-05-12 14:40:19 +0700

    update animation

--- components/common/Animation/index.js
+++ components/common/Animation/index.js
@@ -3,20 +3,23 @@ import classNames from "classnames"
 import { InView } from "react-intersection-observer"
 
 const Animation = ({ children, className }) => (
-  <InView triggerOnce={true}>
-    {({ inView, ref }) => {
-      return (
-        <div
-          ref={ref}
-          className={`inView ${className} ${classNames({
-            inView: true,
-            invisible: inView,
-          })}`}
-        >
-          {children}
-        </div>
-      )
-    }}
-  </InView>
+  // <InView triggerOnce={true}>
+  //   {({ inView, ref }) => {
+  //     return (
+  //       <div
+  //         ref={ref}
+  //         className={`inView ${className} ${classNames({
+  //           inView: true,
+  //           invisible: inView,
+  //         })}`}
+  //       >
+  //         {children}
+  //       </div>
+  //     )
+  //   }}
+  // </InView>
+  <div>
+    {children}
+  </div>
 )
 export default Animation
